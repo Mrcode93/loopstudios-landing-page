2de31b9fe53111472832895553da7907fa53cdb9
Mrcode93 <alhlaqalhlaq93@gmail.com>
2022-09-13 03:44:46 +0300

    v1.1

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 let hamburger = document.querySelector(".hamburger");
 let humImg = document.querySelector(".hamburger img");
 let menu = document.querySelector(".slider");
+let links = document.querySelectorAll("a");
+
+links.forEach((e) => {
+    e.addEventListener("click", (link) => {
+        if (menu.classList.contains("active")) {
+            menu.classList.remove("active");
+            humImg.src = "images/icon-hamburger.svg";
+        }
+    });
+});
 
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
